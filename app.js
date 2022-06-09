c5b25fc724a3bf3b0ab8b4391d1daac3d8ebd3e8
CougarChan <gohan0136@gmail.com>
2022-06-09 15:59:13 -0400

    update

---
+++ app.js
@@ -0,0 +1,18 @@
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const db = require('./db/connections');
+
+function FirstQuestions(){
+  return inquirer
+  .prompt([
+   { type: "list"
+ 
+ }
+
+
+
+
+  ])
+
+
+}
